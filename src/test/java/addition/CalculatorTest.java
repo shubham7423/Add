7709b18f2f56a08f469aa9637413aae754926a78
shubhampatel <sp246531@gmail.com>
2021-02-04 23:33:01 +0530

    javadoc

--- src/test/java/addition/CalculatorTest.java
+++ src/test/java/addition/CalculatorTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Calculator Test
+ * @author shubhampatel
+ *
+ */
 public class CalculatorTest {
 
+	
 	@Test
 	public void test_add_1() {
 		Calculator calculator = new Calculator();
@@ -14,24 +20,27 @@ public class CalculatorTest {
 		assertEquals(30, calculator.addNum());
 	}
 	
+	
 	@Test
-	public void test_add_2() {
+	public void test_add_() {
 		Calculator calculator = new Calculator();
 		calculator.a = 20;
 		calculator.b = 10;
 		assertEquals(30, calculator.addNum());
 	}
 	
+	
 	@Test
-	public void test_add_3() {
+	public void test_sub_1() {
 		Calculator calculator = new Calculator();
 		calculator.a = 40;
 		calculator.b = 10;
 		assertEquals(30, calculator.subtract());
 	}
 	
+	
 	@Test
-	public void test_add_4() {
+	public void test_sub_2() {
 		Calculator calculator = new Calculator();
 		calculator.a = 10;
 		calculator.b = 20;
